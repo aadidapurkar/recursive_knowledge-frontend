6bfcd9bcfd451d056833590bb6dc11680ecff982
Aadi Dapurkar <aadidapurkar04@gmail.com>
2025-09-16 03:39:53 +1000

    added feature to view - steps away from root

--- src/render.ts
+++ src/render.ts
@@ -6,14 +6,18 @@ export const render = (s : State) => {
   const topicContainer = document.getElementById("topicContainer")! 
   const subtopicContainer = document.getElementById("subtopicContainer")! 
   const topicText = document.getElementsByClassName("topicText")[0]!
+  const stps = document.getElementById("stps")! 
+  const stepsText = document.getElementById("stepsAway")! 
 
   const topicChangeRequired : boolean = s.currTopic !== undefined && s.currTopic >= 0 && s.currTopic <= s.topics.length - 1 //&& s.currTopic != parseInt(topicText.id)
 
   if (s.currTopic !== undefined) {
     topicContainer.style.visibility = "visible"
+    stps.style.visibility = "visible"
   }
   // heading
   if(topicChangeRequired) {
+    stepsText.textContent = `${s.currTopic!}`
     topicText.textContent = s.topics[s.currTopic!].title
     topicText.id = `${s.currTopic!}`
   }
