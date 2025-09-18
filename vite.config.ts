528f1c7e4062baf3c8a5bfffc29c19a503bf74ad
Aadi Dapurkar <aadidapurkar04@gmail.com>
2025-09-18 17:02:19 +1000

    minor

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
-  //base: '/wikipedia-explore/', 
+  // base: './',
+  base: '/wikipedia-explore/', 
   build: {
     outDir: 'dist',
   },
